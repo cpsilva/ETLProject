b952db21f728ac86fc2ae732748cf21af212dfbc
cpsilva <caiopirees@gmail.com>
2018-09-18 16:32:08 -0300

    Primeira versão dos metodos para realizar a busca extração e carregamento das informações e implementado metodo para realizar a requisção ao servidor ftp

--- CsvToSql.ETL/CsvToSql.ETL/Program.cs
+++ CsvToSql.ETL/CsvToSql.ETL/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 
 namespace CsvToSql.ETL
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			BaixarCsvViaFTP();
+			TratarInformacoesCsv();
+			GravarInformacaoBancoDados();
+			SalvarCopiaCsvComResultadoS3();
+		}
+
+		private static void BaixarCsvViaFTP()
+		{
+			FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://www.contoso.com/test.htm");
+			request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+			// Credenciais para logar.
+			request.Credentials = new NetworkCredential("anonymous", "user");
+
+			FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+
+			Stream responseStream = response.GetResponseStream();
+			StreamReader reader = new StreamReader(responseStream);
+			Console.WriteLine(reader.ReadToEnd());
+
+			reader.Close();
+			response.Close();
+		}
+
+		private static void TratarInformacoesCsv()
+		{
+
+		}
+
+		private static void GravarInformacaoBancoDados()
+		{
+
+		}
+
+		private static void SalvarCopiaCsvComResultadoS3()
+		{
+
 		}
 	}
 }
