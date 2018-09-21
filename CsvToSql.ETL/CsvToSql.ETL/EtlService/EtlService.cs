955d2362b8ebf66a9e56bb7ad29d138dc6d13698
cpsilva <caiopirees@gmail.com>
2018-09-21 17:40:24 -0300

    remoção do comentario da rotina para executar o save das informações tratadas do csv

--- CsvToSql.ETL/CsvToSql.ETL/EtlService/EtlService.cs
+++ CsvToSql.ETL/CsvToSql.ETL/EtlService/EtlService.cs
@@ -24,7 +24,7 @@ namespace CsvToSql.ETL.EtlService
 		{
 			var arquivo = _ftpService.BaixarCsvViaFTP();
 			var listaTratadaEOrdenada = _csvService.TratarInformacoesCsv(arquivo);
-			//_sqlService.GravarListaDeCampanhasAgrupadasEOrdenadas(listaTratadaEOrdenada);
+			_sqlService.GravarListaDeCampanhasAgrupadasEOrdenadas(listaTratadaEOrdenada);
 			_amazonService.SalvarCopiaCsvComResultadoS3(listaTratadaEOrdenada);
 		}
 	}
